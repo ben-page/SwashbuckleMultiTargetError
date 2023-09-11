2eac2f70991c9471f805460d861ef95c344c8f17
Ben Page <ben.page@csat.com>
2023-09-11 14:59:28 -0500

    demonstration

---
+++ SwashbuckleMultiTarget/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace SwashbuckleMultiTarget
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication ConfigureForDevelopment(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            return app;
+        }
+    }
+}
---
+++ SwashbuckleSingleTarget/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace SwashbuckleMultiTargetError
+{
+    public static class SwashbuckleSingleTarget
+    {
+        public static WebApplication ConfigureForDevelopment(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            return app;
+        }
+    }
+}
